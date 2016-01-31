2408197fcddc9750edfb9ae3f6d731bcc9eb871c
Daniel de la Cruz <daniel.delacruz@scmspain.com>
2016-01-31 22:38:39 +0100

    refactor

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
+
+var resolve = require('resolve');
 var path = require('path');
-var fs = require('fs');
 
-module.exports = function (url, file, done) {
-  if (url[0] === '~') {
-    done({file: recursiveFind(url.replace('~', ''))});
+module.exports = function(url, file, done) {
+  var object;
+
+  if( url[0] === '~' ) {
+
+    var route = url.split(path.sep);
+    var fileImport = route.pop();
+    fileImport = fileImport[0] === '_' ? fileImport : '_' + fileImport;
+    fileImport = fileImport.indexOf('.scss') !== -1 ? fileImport : fileImport + '.scss';
+
+    url = route.concat([fileImport]).join(path.sep);
+
+    object = {
+      file: resolve.sync(url.slice(1), { basedir: __dirname + '/../src'})
+    };
   } else {
-    return done({file: url});
+    object = { file: url };
   }
+
+  return done( object );
 };
-
-function recursiveFind(url) {
-  const urlTree = url.split(path.sep);
-  urlTree.pop();
-  const folder = path.join(__dirname, '../node_modules', urlTree.join(path.sep));
-
-  if (folder === path.join(path.sep + urlTree.join(path.sep))) {
-    return '';
-  }
-
-  if (!fs.existsSync(folder)) {
-    return recursiveFind(path.join('../', url));
-  } else {
-    return path.join('../node_modules', url);
-  }
-}
